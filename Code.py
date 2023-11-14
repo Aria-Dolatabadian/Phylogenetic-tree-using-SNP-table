dc510bf654cdb343cd1292cfb0ae75070f42500a
Aria Dolatabadian <74664712+Aria-Dolatabadian@users.noreply.github.com>
2023-11-14 14:04:18 +0800

    Create Code.py

---
+++ Code.py
@@ -0,0 +1,34 @@
+
+import pandas as pd
+from Bio import Phylo, SeqIO
+from Bio.Seq import Seq
+from Bio.SeqRecord import SeqRecord
+from Bio.Align import MultipleSeqAlignment
+from Bio.Phylo.TreeConstruction import DistanceCalculator, DistanceTreeConstructor
+
+# Read SNP data from CSV
+snp_data = pd.read_csv('snp_data.csv')
+
+# Convert SNP data to a format suitable for phylogenetic analysis
+sequences = []
+for index, row in snp_data.iterrows():
+    sequence = ''.join(row)
+    record = SeqRecord(Seq(sequence), id=f'Individual_{index}')
+    sequences.append(record)
+
+# Create a MultipleSeqAlignment object
+alignment = MultipleSeqAlignment(sequences)
+
+# Calculate distance matrix using 'blosum62' model
+calculator = DistanceCalculator('blosum62')
+dm = calculator.get_distance(alignment)
+
+# Construct the phylogenetic tree
+constructor = DistanceTreeConstructor(calculator)
+tree = constructor.upgma(dm)
+
+# Save the tree to a Newick file
+Phylo.write(tree, 'phylogenetic_tree.nwk', 'newick')
+
+
+#Visualise .nwk  using  https://itol.embl.de/
